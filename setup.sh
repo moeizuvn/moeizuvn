b8efda86725c56eb18ce9a6884a5429ad3b16595
moeizuvn <166965104+moeizuvn@users.noreply.github.com>
2024-08-10 21:59:32 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ echo Please wait for installing...
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o lite10.qcow2 https://app.vagrantup.com/daibangcam/boxes/windowsQCOW/versions/3.0/providers/qemu.box
-echo "Windows 10 x64 Lite On Google Colab"
+curl -L -o lite7.qcow2 https://app.vagrantup.com/daibangcam/boxes/windowsQCOW/versions/2.0/providers/qemu.box
+echo "Windows 7 x86 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
@@ -28,4 +28,4 @@ echo Cell: +84978.39.41.43
 echo Email: daibangcam@gmail.com
 echo Website: hano.cf
 echo ================================
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
